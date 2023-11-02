2ac4b6044617b29bfa53354468e098faff6a5aaa
mdxshakil <mdxshakil1767@gmail.com>
2023-11-02 22:20:46 +0600

    drag and drop feature implemented

---
+++ src/constants/index.ts
@@ -0,0 +1,43 @@
+export type Item = {
+  id: number;
+  name: string;
+};
+
+export const items = [
+  {
+    id: 1,
+    name: "Shirt",
+  },
+  {
+    id: 2,
+    name: "Pant",
+  },
+  {
+    id: 3,
+    name: "Shoes",
+  },
+  {
+    id: 4,
+    name: "Sunglass",
+  },
+  {
+    id: 5,
+    name: "T-Shirt",
+  },
+  {
+    id: 6,
+    name: "Jacket",
+  },
+  {
+    id: 7,
+    name: "Suit",
+  },
+  {
+    id: 8,
+    name: "Watch",
+  },
+  {
+    id: 9,
+    name: "Cap",
+  },
+];
